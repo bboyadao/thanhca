fc53cc0ac6ce40bb2bd7c33fda181fb34022c12f
Nguyen Tuan Anh <xiaoma@Nguyens-MacBook-Pro.local>
2020-02-25 01:58:09 +0700

    add settings

--- thanhca/settings.py
+++ thanhca/settings.py
@@ -125,6 +125,15 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 GRAPH_MODELS = {
-  'all_applications': True,
-  'group_models': True,
+    'all_applications': True,
+    'group_models': True,
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+
+    ]
 }
--- thanhca/urls.py
+++ thanhca/urls.py
@@ -19,7 +19,19 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
-schema_view = get_schema_view(
+admin_view = get_schema_view(
+    openapi.Info(
+        title="Thanhca Admin API",
+        default_version='v1',
+        description="Api For Amin",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="bboyadao@gmail.com"),
+        license=openapi.License(name="Thanhca"),
+    ),
+    public=False,
+    permission_classes=(permissions.IsAdminUser,),
+)
+client_view = get_schema_view(
     openapi.Info(
         title="Thanhca Admin API",
         default_version='v1',
@@ -29,16 +41,18 @@ schema_view = get_schema_view(
         license=openapi.License(name="Thanhca"),
     ),
     public=True,
-    permission_classes=(permissions.IsAdminUser,),
+    permission_classes=(permissions.AllowAny,),
 )
 
 api_docs = [
-    re_path('api/docs/swagger(?P<format>\.json|\.yaml)$',
-            schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path('api/docs/swagger$', schema_view.with_ui('swagger',
-                                                     cache_timeout=0), name='schema-swagger-ui'),
-    re_path('api/docs/redoc/$', schema_view.with_ui('redoc',
-                                                    cache_timeout=0), name='schema-redoc'),
+    # re_path('docs/swagger(?P<format>\.json|\.yaml)$',
+    #         schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    # re_path('docs/swagger$', schema_view.with_ui('swagger',
+    #                                                  cache_timeout=0), name='schema-swagger-ui'),
+    re_path('docs/admin/$', admin_view.with_ui('redoc',cache_timeout=0), name='admin-docs'),
+    re_path('docs/client/$', client_view_view.with_ui('redoc',cache_timeout=0), name='client-docs'),
+
+
 ]
 
 
